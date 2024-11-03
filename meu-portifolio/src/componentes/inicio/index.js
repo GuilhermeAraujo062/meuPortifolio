21b7012ab3707a6d49c98909a39db21a65f87334
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2024-11-03 15:25:07 -0300

    Primeria section finalizada

--- meu-portifolio/src/componentes/inicio/index.js
+++ meu-portifolio/src/componentes/inicio/index.js
@@ -4,13 +4,15 @@ const Inicio = () => {
     return (
         <section className='inicio'>
             <div className='titulo'>
-                <h1>Olá, eu sou<br/> Guilherme Araujo<br/>Desenvolvedor Front-End</h1>
-                <button>Github</button>
-                <button>Linkedin</button>
+                <h1>Olá, eu sou<br/> <span>Guilherme Araujo</span><br/>Desenvolvedor Front-End</h1>
+                <div className='btn'>
+                    <a href="https://github.com/GuilhermeAraujo062" target='_blank'>Github</a>
+                    <a href="https://www.linkedin.com/in/guilherme62araujo/" target='_blank'>Linkedin</a>
+                </div>
             </div>
 
             <div className='img'>
-                <img src="./imagens/person-picture.png"></img>
+                <img src="./imagens/person-picture.png" alt='person'></img>
             </div>
             
         </section>
